928678eea7a2dcb675f263274820225e02ec12a5
egigoka <egigoka@gmail.com>
2026-07-02 16:27:17 +0500

    Add DockDoor patched cask

---
+++ Casks/dockdoor-patched.rb
@@ -0,0 +1,25 @@
+cask "dockdoor-patched" do
+  version "1.39.3-patched"
+  sha256 "983e94cf559e5e676b045d4eb011eea5bde4842ed37d0e1f2ddcda0201303468"
+
+  url "https://github.com/egigoka/DockDoor/releases/download/#{version}/DockDoor-patched.dmg"
+  name "DockDoor Patched"
+  desc "DockDoor with mouse cursor movement code removed"
+  homepage "https://github.com/egigoka/DockDoor"
+
+  livecheck do
+    url :url
+    regex(/^(\d+(?:\.\d+)+-patched)$/i)
+  end
+
+  auto_updates true
+
+  app "DockDoor.app"
+
+  zap trash: [
+    "~/Library/Application Support/DockDoor",
+    "~/Library/Caches/com.ethanbills.DockDoor",
+    "~/Library/HTTPStorages/com.ethanbills.DockDoor",
+    "~/Library/Preferences/com.ethanbills.DockDoor.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-casks-patched
+
+Personal Homebrew tap for patched casks.
+
+## Usage
+
+```sh
+brew tap egigoka/casks-patched
+brew install --cask dockdoor-patched
+```
